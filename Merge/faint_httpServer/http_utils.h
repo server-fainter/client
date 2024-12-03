a0ebe8072195c741c03e4bce08c8f3c44193ca6e
eunhye <gp79810@gmail.com>
2024-12-04 03:45:59 +0900

    [정리]

--- Merge/faint_httpServer/http_utils.h
+++
@@ -1,8 +0,0 @@
-//http통신 관리 함수 선언
-#define HTTP_UTILS_H
-
-// HTTP 응답 생성 함수 선언
-void send_response(int client_socket, const char *status, const char *body);
-
-// POST 요청 처리 함수 선언
-void handle_post_request(int client_socket, const char *data);
